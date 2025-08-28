c0cf63ba6821975548b5ccfcd04fec2e423d21ba
Roberto Esposito <roberto.esposito@unito.it>
2025-08-28 10:12:50 +0200

    first import

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A flake for developing a neovim plugin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        neovim = pkgs.neovim;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            neovim
+            luajit
+            luarocks
+            stylua
+          ];
+
+          shellHook = ''
+            export XDG_CONFIG_HOME=$(pwd)/.config
+            echo "Nix dev shell configured to use local nvim config"
+          '';
+        };
+      }
+    );
+}
---
+++ lua/marp-dev-preview.nvim/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.goto = function()
+  print("hello world")
+end
+
+return M
