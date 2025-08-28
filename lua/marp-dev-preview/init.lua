d690d655e19fc0d6e6e4ee7e7283f503056e673f
Roberto Esposito <roberto.esposito@unito.it>
2025-08-28 13:01:53 +0200

    initial import

--- lua/marp-dev-preview/init.lua
+++ lua/marp-dev-preview/init.lua
@@ -1,8 +1,23 @@
 local M = {}
+local H = {
+  config = {
+    live_sync = false
+  }
+}
 
-M.compute_slide_number = function()
-  local slide_number = 0
-  for line in vim.api.nvim_buf_get_lines(0, 0, -1, false) do
+M.setup = function(config)
+  if not config then
+    config = {}
+  end
+
+  -- updates default_config with user options
+  for k,v in pairs(config) do H.config[k] = v end
+end
+
+M.current_slide_number = function()
+  local slide_number = -1
+  local cur_line = vim.api.nvim_win_get_cursor(0)[1]
+  for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, cur_line, false)) do
     if line.sub(line,1,3)=="---" then
       slide_number = slide_number + 1
     end
@@ -19,16 +34,30 @@ M.server_cmd = function(cmd,arg)
   return response
 end
 
-M.goto = function()
-  local input = vim.fn.input("Slide number: ")
-  local slide_number = tonumber(input)
+M._goto = function(slide_number)
   if slide_number then
     local response = M.server_cmd("goto", { key = "slide", value = slide_number })
     if response.status == 200 then
-      vim.notify("Went to slide " .. slide_number, vim.log.levels.INFO)
+      vim.notify("Went to slide " .. slide_number, vim.log.levels.DEBUG)
     else
       vim.notify("Failed to go to slide: " .. response.body, vim.log.levels.ERROR)
     end
+  end
+end
+
+M.goto = function()
+  local input = vim.fn.input("Slide number: ")
+  local slide_number = tonumber(input)
+  M._goto(slide_number)
+end
+
+M._last_slide_number = nil
+
+M.goto_current_slide = function()
+  local slide_number = M.current_slide_number()
+  if slide_number ~= M._last_slide_number then
+    M._last_slide_number = slide_number
+    M._goto(slide_number)
   end
 end
 
@@ -43,5 +72,22 @@ M.find = function()
     end
   end
 end
+
+
+M.toggle_live_sync = function()
+  H.config.live_sync = not H.config.live_sync
+end
+
+vim.api.nvim_create_augroup("SlideSync", { clear = true })
+
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = "SlideSync",
+  pattern = "*.md",
+  callback = function()
+    if H.config.live_sync then
+      M.goto_current_slide()
+    end
+  end
+})
 
 return M
--- plugin/marp-dev-preview.nvim.vim
+++ plugin/marp-dev-preview.nvim.vim
@@ -19,3 +19,5 @@ exe "lua package.path = package.path .. ';" . s:plenary_path . "/?/init.lua'"
 
 " Exposes the plugin's functions for use as commands in Neovim.
 command! -nargs=0 MarpDevPreviewGoTo lua require("marp-dev-preview").goto()
+command! -nargs=0 MarpDevPreviewToggleLiveSync lua require("marp-dev-preview").toggle_live_sync()
+command! -nargs=0 MarpDevPreviewFind lua require("marp-dev-preview").find()
